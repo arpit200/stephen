3b0936ad973b957f21433c4dd178cac5a581ba8e
arpit200 <69860102+arpit200@users.noreply.github.com>
2022-01-30 15:32:53 +0530

    Intro

---
+++ python1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 15:19:25 2022
+
+@author: STEPHEN ARPIT
+"""
+Thapar_batch = [1,2,3,4,5,6,7,8]
+print(Thapar_batch)
+import matplotlib.pyplot as pt
+pt.plot(Thapar_batch)
